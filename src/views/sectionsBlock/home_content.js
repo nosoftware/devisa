a1ac5e594038f4815e9c9f1230e71d8f68d8484a
trilokesh naskar <trilokeshnaskar@gmail.com>
2021-08-26 13:36:22 +0530

    jgsfh

---
+++ src/views/sectionsBlock/home_content.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from "react";
+import { config } from '../../constant';
+import axios from "axios";
+
+/*import {
+  // Button,
+  Container,
+  Row,
+  Col,
+  Media,
+} from "reactstrap";*/
+
+//const getPageContentUrl = "http://localhost/admin/api/get-page";
+//const getPageContentUrl = "http://my-demo.xyz/visa/api/get-page";
+const getPageContentUrl = config.url.API_URL+"/get-page";
+
+function HomeContent() {
+	const [pageContent, setPageContentData] = useState('content');
+
+	const getPageContentWithAxios = async () => {
+		const res = await axios.get(getPageContentUrl,{ params: { page: 'home' }});
+		//console.log(response);
+		const pageContent = res.data.content;
+		setPageContentData(pageContent);
+		//console.log(pageContent);
+	};
+
+	useEffect(() => {
+		getPageContentWithAxios();
+	}, []);
+	
+	return (
+    <>
+		<div className="section section-step" dangerouslySetInnerHTML={{ __html: pageContent[0].content }}></div>
+
+    </>
+  );
+}
+
+export default HomeContent;
+
